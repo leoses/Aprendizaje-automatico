f47ca8d398e6f4662988fa615f2841e70796cd1a
Paula Martínez <pamart24@ucm.es>
2021-09-20 12:56:46 +0200

    Arreglá la gráfica de bucles

--- práctica_00/práctica_00.py
+++ práctica_00/práctica_00.py
@@ -29,13 +29,12 @@ def integra_mc_2(fun, a, b, num_puntos=10000):
     max = fun(a)
     for i in range(b): ##remember for num in nums if num < 0
         if(max < fun(i)): max = fun(i)
-        i = i + 1
     
     i = 0
     puntosX = []
     puntosY = []
     for i in range(num_puntos):
-        puntosX.append(random.random() * (b-a) + a )
+        puntosX.append(random.random() * (b-a) + a)
         puntosY.append(random.random() * max)
 
     puntosFuncionY = []
@@ -51,30 +50,34 @@ def integra_mc_2(fun, a, b, num_puntos=10000):
 
 
 def pinta_grafica():
+    masPunticos = 20000
+    puntosTotales = 10000
     sizes = np.linspace(100,10000000,20)
     tiempo_bucle = []
     tiempo_np = []
 
     for size in sizes:
-        x1 = random.randint(1000,10000)
-        x2 = random.randint(1000,10000)
+        x1 = random.randint(1,100)
+        x2 = random.randint(1,100)
 
         if x1 > x2 : x1,x2 = x2,x1
 
         time_ini = time.process_time()
-        integra_mc(cuadrado,x1,x2)
+        integra_mc(cuadrado, x1, x2, puntosTotales)
         tiempo_np += [1000*(time.process_time() - time_ini)]
         
         time_ini = time.process_time()
-        integra_mc_2(cuadrado, x1,x2)
+        integra_mc_2(cuadrado, x1, x2, puntosTotales)
         tiempo_bucle += [1000*(time.process_time() - time_ini)]
+
+        puntosTotales += masPunticos
     
     plt.figure()
     plt.scatter(sizes,tiempo_bucle,c = 'red',label='bucle')
     plt.scatter(sizes,tiempo_np,c = 'blue',label='numpy')
     plt.legend()
+    plt.savefig('time.png')
     plt.show()
-    plt.savefig('time.pdf')
 
 #area = integra_mc(cuadrado, 0, 20000)
 #x1 = np.random.uniform(1,100)
