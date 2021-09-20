a2276780d74049c94e1f3d8659b0b82b89086b55
leoses <48771457+leoses@users.noreply.github.com>
2021-09-20 11:57:09 +0200

    ADD: se pintan cosas pero rectas xDD
    Co-Authored-By: Paula <48999523+pamart24@users.noreply.github.com>

--- práctica_00/práctica_00.py
+++ práctica_00/práctica_00.py
@@ -1,5 +1,9 @@
 import numpy as np
 import scipy.integrate
+import random
+
+import time
+import matplotlib.pyplot as plt
 
 #funcion de prueba
 def cuadrado(x):
@@ -7,7 +11,7 @@ def cuadrado(x):
 
 #no iterativo
 def integra_mc(fun, a, b, num_puntos=10000):
-    puntos =np.linspace(a, b+1, b-a)
+    puntos = np.linspace(a, b+1, b-a)
     max = np.max(fun(puntos))
 
     puntosX = (np.random.rand(num_puntos) * (b-a)) + a 
@@ -26,14 +30,58 @@ def integra_mc_2(fun, a, b, num_puntos=10000):
     for i in range(b): ##remember for num in nums if num < 0
         if(max < fun(i)): max = fun(i)
         i = i + 1
-
     
+    i = 0
+    puntosX = []
+    puntosY = []
+    for i in range(num_puntos):
+        puntosX.append(random.random() * (b-a) + a )
+        puntosY.append(random.random() * max)
 
+    puntosFuncionY = []
+    i = 0
+    for i in range(num_puntos):
+        puntosFuncionY.append(fun(puntosX[i]))
     
+    cont = 0
+    for i in range(num_puntos):
+        if puntosY[i] < puntosFuncionY[i] : cont += 1
     
-    return 0
+    return ((max * (b-a)) * (cont /num_puntos))
 
-area = integra_mc(cuadrado, 10, 20 )
-print(area)
-area2 = scipy.integrate.quad(cuadrado, 10,20)
-print(area2)
+
+def pinta_grafica():
+    sizes = np.linspace(100,10000000,20)
+    tiempo_bucle = []
+    tiempo_np = []
+
+    for size in sizes:
+        x1 = random.randint(1000,10000)
+        x2 = random.randint(1000,10000)
+
+        if x1 > x2 : x1,x2 = x2,x1
+
+        time_ini = time.process_time()
+        integra_mc(cuadrado,x1,x2)
+        tiempo_np += [1000*(time.process_time() - time_ini)]
+        
+        time_ini = time.process_time()
+        integra_mc_2(cuadrado, x1,x2)
+        tiempo_bucle += [1000*(time.process_time() - time_ini)]
+    
+    plt.figure()
+    plt.scatter(sizes,tiempo_bucle,c = 'red',label='bucle')
+    plt.scatter(sizes,tiempo_np,c = 'blue',label='numpy')
+    plt.legend()
+    plt.show()
+    plt.savefig('time.pdf')
+
+#area = integra_mc(cuadrado, 0, 20000)
+#x1 = np.random.uniform(1,100)
+#print(type(x1))
+
+#print(area)
+#area2 = scipy.integrate.quad(cuadrado, 0,20)
+#print(area2)
+#print(type(area))
+pinta_grafica()
