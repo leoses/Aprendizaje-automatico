3f3a9191ea24d345006683541781e42b7f95e39f
leoses <48771457+leoses@users.noreply.github.com>
2021-10-06 11:39:51 +0200

    Me quiero morir pero esta terminado :D :100: :fire:
    Co-Authored-By: Paula <48999523+pamart24@users.noreply.github.com>

--- práctica_01/práctica01.py
+++ práctica_01/práctica01.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.fromnumeric import transpose
 from numpy.core.numeric import empty_like
 from pandas.io.parsers import read_csv
 import matplotlib.pyplot as plt
@@ -40,7 +41,7 @@ def regresion_lineal_1():
     plt.plot([min_x,max_x], [min_y,max_y],color='blue', linewidth=2)
     plt.show()
 
-def regresion_lineal_2():
+def regresion_lineal_2(alpha = 0.01):
     datos = carga_csv('ex1data2.csv')
 
     X = datos[:, :-1]
@@ -49,12 +50,34 @@ def regresion_lineal_2():
     m = np.shape(X)[0]
     n = np.shape(X)[1]
     
+    #Normalizamos la matriz X
     X_normal = normaliza(X)
     #Añadimos una columna de 1's
     X_normal[0] = np.hstack([np.ones([m, 1]), X_normal[0]])
 
-    alpha = 0.01
+    #Aplicamos el descenso del gradiente
     Theta, Costes = descenso_gradiente(X_normal[0],Y,alpha)
+
+    #Aqui iría la llamada a la función de pintar la grafica que nos pidiesen
+    me_quiero_morir_3(Costes)
+
+    return Theta, X_normal
+
+def regresion_lineal_3(alpha = 0.01):
+    datos = carga_csv('ex1data2.csv')
+
+    X = datos[:, :-1]
+    Y = datos[:, -1]
+
+    m = np.shape(X)[0]
+    n = np.shape(X)[1]
+    
+    #Añadimos una columna de 1's
+    X= np.hstack([np.ones([m, 1]), X])
+
+    Theta = normal(X,Y)
+
+    return Theta
 
     
 
@@ -94,11 +117,11 @@ def me_quiero_morir_v2(cosos):
     ax.contour(cosos[0], cosos[1], cosos[2], np.logspace(-2,3,20),cmap= plt.get_cmap('rainbow'))
     plt.show()
 
-def me_quiero_morir_3(cosos):
-    #Hacer grafiquita de alpha (parte 2)
+def me_quiero_morir_3(coste):
     fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.contour(cosos[0], cosos[1], cosos[2], np.logspace(-2,3,20),cmap= plt.get_cmap('rainbow'))
+    ite = 1500
+    x = np.linspace(0, ite, ite, endpoint = True)
+    plt.plot(x, coste)
     plt.show()
     
 def normaliza(matriz):
@@ -122,8 +145,6 @@ def descenso_gradiente(X, Y, alpha, iterations = 1500):
         Theta = gradiente(X,Y, Theta, alpha)
         Coste[i] = coste2(X,Y,Theta)
 
-    #Aqui iría la llamada a la función de pintar la grafica que nos pidiesen
-
     return Theta, Coste
 
 def gradiente(X, Y, Theta, alpha):
@@ -137,8 +158,25 @@ def gradiente(X, Y, Theta, alpha):
         theta_nueva[i] -= (alpha / m) * aux_i.sum()
     return theta_nueva
 
+def normal(X,Y):
+    transpuesta_x = np.transpose(X)
+    Aux = np.linalg.inv(np.dot(transpuesta_x,X))
+    return np.dot(np.dot(Aux, transpuesta_x), Y)
+
 #regresion_lineal_1()
 #datos = carga_csv('ex1data2.csv')
 #me_quiero_morir(make_data([-10,10],[-1,4],datos[:,0],datos[:,1]))
 #print(normaliza(datos))
-regresion_lineal_2()
+theta_2, x_normal = regresion_lineal_2()
+theta_3 = regresion_lineal_3()
+
+X = [1650,3]
+X_theta2 = (X - x_normal[1])/x_normal[2]
+
+X = np.insert(X, 0, 1)
+X_theta2 = np.insert(X_theta2, 0, 1)
+
+print("Coste de descenso de gradiente: " )
+print(np.dot(np.transpose(theta_2),X_theta2))
+print("Coste por ecuación normal: ")
+print(np.dot(np.transpose(theta_3),X))
