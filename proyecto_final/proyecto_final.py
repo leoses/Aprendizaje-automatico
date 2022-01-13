6f161e1773bc43bd7007b41342be4dc0c2257404
leoses <48771457+leoses@users.noreply.github.com>
2022-01-13 19:42:10 +0100

    ADD: chupate esa redes neuronales
    Co-Authored-By: Paula <48999523+pamart24@users.noreply.github.com>

--- proyecto_final/proyecto_final.py
+++ proyecto_final/proyecto_final.py
@@ -6,7 +6,7 @@ from numpy.random import normal
 import sklearn.svm as svm
 from sklearn.metrics import accuracy_score
 from pandas.io.parsers import read_csv
-from scipy.optimize import fmin_tnc
+from scipy.optimize import fmin_tnc, minimize
 from sklearn.preprocessing import PolynomialFeatures
 
 def visualize_boundary(X, y, svm, file_name):
@@ -440,6 +440,138 @@ def regresion_logistica_reg():
     print("Mejores resultados para los ejemplos de validacion: landa = " + str(landa_score_val) + " con un porcentaje de acierto de " + str(best_score_val) + " %")
     print("Mejores resultados para los ejemplos de test: landa = " + str(landa_score_training) + " con un porcentaje de acierto de " + str(best_score_training) + " %")
 
+def calcula_propagacion(X, theta1, theta2):
+    m = np.shape(X)[0]
+    A1 = np.hstack([np.ones([m, 1]), X])
+    Z2 = np.dot(A1, theta1.T)
+    A2 = np.hstack([np.ones([m, 1]), func_sigmoid(Z2)])
+    Z3 = np.dot(A2, theta2.T)
+    A3 = func_sigmoid(Z3)
+    return A1, A2, A3
+
+def gradiente_regularizacion(gradiente, m, reg, theta):
+    columnaGuardada = gradiente[0]
+    gradiente = gradiente + (reg/m)*theta
+    gradiente[0] = columnaGuardada
+    return gradiente
+
+def coste_v2(X, y, t1, t2):
+    m = np.shape(X)[0]
+    a1, a2, h = calcula_propagacion(X, t1, t2)
+    return (1/m)*np.sum(-y*np.log(h) - (1-y)*np.log(1-h))
+
+def coste_reg_v2(X, y, t1, t2, reg):
+    m = np.shape(X)[0]
+    #En el coste reg no hay que incluir la primera columna de t1 y t2
+    otra = (reg/(2*m)) * (np.sum(np.power(t1[1:], 2)) + np.sum(np.power(t2[1:], 2)))
+    return coste_v2(X, y, t1, t2) + otra
+
+def backprop(params_rn, num_entradas, num_ocultas, num_etiquetas, X, Y, reg):
+ 
+    m = np.shape(X)[0]
+
+    Theta1 = np.reshape(
+        params_rn[:num_ocultas * (num_entradas + 1)], (num_ocultas, (num_entradas+1)))
+    Theta2 = np.reshape(
+        params_rn[num_ocultas * (num_entradas + 1):], (num_etiquetas, (num_ocultas+1)))
+ 
+    # Calculamos propagacion
+    A1, A2, H = calcula_propagacion(X, Theta1, Theta2)
+    Delta1 = np.zeros_like(Theta1)
+    Delta2 = np.zeros_like(Theta2)
+
+    for t in range(m):
+        a1t = A1[t, :] 
+        a2t = A2[t, :] 
+        ht = H[t, :] 
+        yt = Y[t]
+ 
+        d3t = ht - yt 
+        d2t = np.dot(Theta2.T, d3t)*(a2t*(1-a2t)) 
+ 
+        Delta1 = Delta1 + np.dot(d2t[1:, np.newaxis], a1t[np.newaxis, :])
+        Delta2 = Delta2 + np.dot(d3t[:, np.newaxis], a2t[np.newaxis, :])
+ 
+
+    gradiente1 = Delta1/m
+    gradiente2 = Delta2/m
+
+    gradiente1 = gradiente_regularizacion(gradiente1, m, reg, Theta1)
+    gradiente2 = gradiente_regularizacion(gradiente2, m, reg, Theta2)
+ 
+    return coste_reg_v2(X, Y, Theta1, Theta2, reg), np.concatenate([np.ravel(gradiente1), np.ravel(gradiente2)])
+
+def y_oneHot(y, numLabels):
+    m = len(y)
+ 
+    y_onehot = np.zeros((m, numLabels))
+ 
+    for i in range(m):
+        y_onehot[i][y[i]] = 1
+ 
+    return y_onehot
+
+def optimize_backprop_and_check (t1, t2,num_entradas, num_ocultas, num_etiquetas, reg, X, y_onehot, laps):
+    
+    eini = 0.77
+    params = np.concatenate([np.ravel(t1),np.ravel(t2)])
+    pesos = np.random.uniform(-eini,eini, params.shape[0])
+ 
+    out = minimize(fun = backprop, x0 = pesos, args = (num_entradas, num_ocultas, num_etiquetas, X, y_onehot, reg), method='TNC', jac = True, options = {'maxiter': laps})
+ 
+    Thetas1 = out.x[:(num_ocultas*(num_entradas+1))].reshape(num_ocultas,(num_entradas+1))
+    Thetas2 = out.x[(num_ocultas*(num_entradas+1)):].reshape(num_etiquetas,(num_ocultas+1))
+   
+    return Thetas1, Thetas2
+    
 
 
-regresion_logistica_reg()
+def redes_neuronales():
+    print("Estamos cargando")
+    df = read_csv('diabetes-dataset.csv', header=0)
+
+    for i in range(1, len(df.columns) - 1):
+        df[df.columns[i]] = df[df.columns[i]].replace({ 0 : df[df.columns[i]].mean() })
+
+    a = df.to_numpy().astype(float)
+    b = df.to_numpy().astype(int)
+    X = a[:,:-1]
+    y = b[:,-1]
+    print("Ya hemos cargado")
+
+    #Ejemplos de entrenamiento
+    entX = X[0:(int)(2000*0.6)]
+    enty = y[0:(int)(2000*0.6)]
+    #Ejemplos de validacion
+    Xval = X[(int)(2000*0.6): (int)(2000*0.9)]
+    yval = y[(int)(2000*0.6): (int)(2000*0.9)]
+    #Ejemplos de test
+    Xtest = X[(int)(2000*0.9):2000]
+    ytest = y[(int)(2000*0.9):2000]
+
+    num_etiquetas = 2
+    auxY = y_oneHot(enty, num_etiquetas)
+    num_ocultas = 1000
+    num_entradas = np.shape(entX)[1]
+    
+    landas = np.array([ 0.00001,0.001,0.01 ,0.03, 0.1, 0.3, 1, 3])
+
+    theta1= np.zeros((num_ocultas, num_entradas+1))
+    theta2 = np.zeros((num_etiquetas, num_ocultas+1))
+
+    for landa in landas:    
+        T1, T2 = optimize_backprop_and_check(theta1, theta2,num_entradas,num_ocultas, num_etiquetas, landa,entX, auxY, 70)
+
+        print("Para landa = " + str(landa))
+        A1,A2,H = calcula_propagacion(Xval,T1, T2)
+        maxIndices = np.argmax(H,axis=1) 
+        acertados = np.sum(maxIndices==yval.ravel())
+        print("Porcentaje acertados validacion: " + str(acertados*100/np.shape(H)[0]) + "%")
+
+        A1,A2,H = calcula_propagacion(Xtest,T1, T2)
+        maxIndices = np.argmax(H,axis=1) 
+        acertados = np.sum(maxIndices==ytest.ravel())
+        print("Porcentaje acertados entrenamiento: " + str(acertados*100/np.shape(H)[0]) + "%")
+
+
+redes_neuronales()
